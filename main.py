fb72b635ed5876edcc1d7e47afd02c20130cc582
hassan ali <hassanali5120@gmail.com>
2021-03-07 16:31:37 +0500

    updated code usb bug testing on mac now

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import traceback
-import time
+from time import sleep
 
+
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
 driver = webdriver.Chrome(
-    'F:\projects\tiktokScrapper\chromedriver.exe')
+    'F:\\projects\\tiktokScrapper\\chromedriver.exe', options=options)
+
 
 driver.get('https://www.tiktok.com/')
+action = ActionChains(driver)
+infulencers = driver.find_elements(By.CSS_SELECTOR, '.author-uniqueId')
+for infulencer in infulencers:
+    action.move_to_element(infulencer).perform()
+    print(infulencer.text)
