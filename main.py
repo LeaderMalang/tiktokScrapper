289a9217e540cc9272a9c9a8e04c5052fa1f066a
hassan ali <hassanali5120@gmail.com>
2021-03-07 15:35:21 +0500

    add selenium driver

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hi")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import traceback
+import time
+
+driver = webdriver.Chrome(
+    '/Users/casper.local/Desktop/Dev/job_listing/chromedriver')
+
+driver.get('https://www.crypto-careers.com/jobs/search?d=&l=&lat=&long=&q=crypto')
